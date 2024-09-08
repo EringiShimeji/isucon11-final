211b348f13bfa86806995e1fa6aadbcf716620f2
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-09-08 21:47:25 +0900

    map への登録のタイミング間違ってた

--- app/go/main.go
+++ app/go/main.go
@@ -647,7 +647,6 @@ func (h *handlers) GetGrades(c echo.Context) error {
 
 		// 講義毎の成績計算処理
 		classScores := make([]ClassScore, 0, len(classes))
-		classScoreMap[course.ID] = classScores
 		for _, class := range classes {
 			// ユーザーのスコア
 			myScore := class.UserScore
@@ -667,6 +666,7 @@ func (h *handlers) GetGrades(c echo.Context) error {
 				Submitters: class.TotalCount,
 			})
 		}
+		classScoreMap[course.ID] = classScores
 	}
 
 	for _, course := range registeredCourses {
